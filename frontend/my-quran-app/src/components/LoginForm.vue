d2e969f3c6827d696868768cc1bb94b337bfd340
¨yunusemre¨ <ttnkntnsmr69@gmail.com>
2023-08-23 07:56:26 +0200

    login form created

---
+++ frontend/my-quran-app/src/components/LoginForm.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="mx-auto mt-15">
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
+    >
+      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+
+      <v-text-field
+        density="compact"
+        v-model="UserData.username"
+        placeholder="User Name"
+        prepend-inner-icon="mdi-email-outline"        
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Password
+
+        <a
+          class="text-caption text-decoration-none text-blue"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Forgot login password?</a>
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        v-model="UserData.password"
+        placeholder="Enter your password"
+        prepend-inner-icon="mdi-lock-outline"        
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <v-card
+        class="mb-12"
+        color="surface-variant"
+        variant="tonal"
+      >        
+      </v-card>
+      <v-btn
+        block
+        class="mb-8"
+        color="black"
+        size="large"
+        variant="tonal"
+      >
+        Log In
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <a
+          class="text-blue text-decoration-none"
+          href="#"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+        </a>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+<script>
+export default{
+    data() {
+      return {
+        UserData: {
+            username: "",
+            password:"",
+        }
+      }  
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ frontend/my-quran-app/src/main.js
@@ -0,0 +1,26 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+          sets: {
+            mdi,
+          },
+        },
+      },
+})
+
+createApp(App).use(store).use(vuetify).use(router).mount('#app')
